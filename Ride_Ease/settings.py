64ad3d51d440d4fce81c0d94b060af7ab8e0d5a2
DakshSammi <97395545+DakshSammi@users.noreply.github.com>
2023-07-02 12:13:14 +0530

    Update settings.py

--- Ride_Ease/settings.py
+++ Ride_Ease/settings.py
@@ -121,7 +121,7 @@ USE_TZ = True
 #  Static files (CSS, JavaScript, Images)
 #https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/staticfiles/'
 <<<<<<< HEAD
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
@@ -130,7 +130,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 MEDIA_URL = '/images/'
 
-MEDIA_ROOT = BASE_DIR/'static'
+MEDIA_ROOT = BASE_DIR/'staticfiles'
 # Default primary key field type
 #https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
